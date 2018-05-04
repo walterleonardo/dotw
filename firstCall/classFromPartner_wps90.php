48f955dc08dd70d4f2c6793263976f116b6c3967
walterleonardo <walterleonardo@gmail.com>
2018-05-04 19:28:50 +0200

    RestReason included

--- firstCall/classFromPartner_wps90.php
+++ firstCall/classFromPartner_wps90.php
@@ -12,7 +12,7 @@ class Input {
     public $customerId = 1237928; //integer 1317257 --- 1237928 CITY 12624 country 81
     public $environment = 'dev'; //string
     public $requestSource = 1; //integer
-    public $restReason = array(1,3); //array integer No mandatory
+    public $restReason = array(1,2,3); //array integer No mandatory
     public $passengerNationalityOrResidenceProvided = true; //boolean
     public $hotelIds = array (); //array(int)
     //country 143 and city 85516
