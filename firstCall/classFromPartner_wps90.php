731d3a01e75518bbc401185bb2fcb1f9f4373ee5
walterleonardo <walterleonardo@gmail.com>
2018-05-09 14:11:58 +0200

    dasdasd

--- firstCall/classFromPartner_wps90.php
+++ firstCall/classFromPartner_wps90.php
@@ -9,14 +9,14 @@ namespace Hotel\PreSupplier;
 //$message="PSFILTER |164|prod|1|Y|14,24,34,44,54,64|||||1~5#5#10~N~N,2~2#3#6~N~N||\r\n";
 class Input {
 
-    public $customerId = 1237928; //integer 1317257 --- 1237928 CITY 12624 country 81
+    public $customerId = 1317257; //integer 1317257 --- 1237928 CITY 12624 country 81
     public $environment = 'dev'; //string
     public $requestSource = 1; //integer
-    public $restReason = array(1,3); //array integer No mandatory
+    public $restReason = array(1,2,3,4,5); //array integer No mandatory
     public $passengerNationalityOrResidenceProvided = true; //boolean
     public $hotelIds = array (); //array(int)
     //country 143 and city 85516
-    public $city = 12764; //12764; //7674; //integer 7674 // 13474 Zamora //12624 BUCHARESt //14 kuwait
+    public $city = 13474; //12764; //7674; //integer 7674 // 13474 Zamora //12624 BUCHARESt //14 kuwait
     public $country = null; //integer 971
     public $bookingChannelsWithAutoMapping = array (
 //            0 => 1011,
@@ -35,7 +35,7 @@ class Input {
     public $bookingChannelTypes = array (); //array(int)
     
     //ExcludedBookingchannel new object
-    public $excludedBookingchannel = array(0 => 1549,); //array(int)
+    public $excludedBookingchannel = array(); //array(int)
     /**
      * @var array of RoomOccupancy
      */
