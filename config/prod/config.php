9fd71b811b7e65812e5ff546bc22f35a4ace7676
walterleonardo <walterleonardo@gmail.com>
2018-05-18 12:48:26 +0200

    dasdasd

--- config/prod/config.php
+++ config/prod/config.php
@@ -6,7 +6,7 @@ class ServersCluster {
     // Server configuration, 'IP ADDRESS','PORT','ENABLED or DISABLED','Internaly used: quantity of fails request before to DISABLE it'
     //IF only want one server config the first one.
     public static $hostsPort =array(
-        array('10.255.5.169', '8003', 'ENABLED', 0),
+        array('10.255.5.78', '8003', 'ENABLED', 0),
         array('10.255.5.74', '8003', 'DISABLED', 0),
         array('10.255.5.202', '8003', 'DISABLED', 0),
         array('10.255.5.212', '8003', 'DISABLED', 0),
