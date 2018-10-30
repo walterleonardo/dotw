c6ab2389ad7ffd3df09cc5c324c73c95c86e2e67
walterleonardo <walterleonardo@gmail.com>
2018-10-30 12:00:56 +0100

    PHASE 2

--- firstCall/MNtransductorClass.php
+++ firstCall/MNtransductorClass.php
@@ -34,7 +34,7 @@ class ArrayChannelCodes {
 /*
  * You need replace this previous require for your objets files
  */
-require 'classFromPartner_wps92_phase_2_wps107.php';
+require 'classFromPartner_wps92_phase_2_wps110.php';
 
 
 if ($platform == 'test')
