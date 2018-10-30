98c1d7c39137462a1a55bfe595200e0214469b3d
walterleonardo <walterleonardo@gmail.com>
2018-10-30 11:06:44 +0100

    PHASE 2

--- config/stage/config.php
+++ config/stage/config.php
@@ -6,8 +6,8 @@ class ServersCluster {
     // Server configuration, 'IP ADDRESS','PORT','ENABLED or DISABLED','Internaly used: quantity of fails request before to DISABLE it'
     //IF only want one server config the first one.
     public static $hostsPort =array(
-        array('10.255.0.202', '10003', 'ENABLED', 0),
-        array('10.255.0.11', '10003', 'DISABLED', 0),
+        array('10.255.0.202', '10003', 'DISABLED', 0),
+        array('10.255.0.11', '10003', 'ENABLED', 0),
     );
 }
 
