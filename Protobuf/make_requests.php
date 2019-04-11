4b07429faaa6a6c13d9a77c60d8c31baf0587d2f
walterleonardo <walterleonardo@gmail.com>
2019-04-11 19:46:14 +0200

    Included protobuf working example

--- Protobuf/make_requests.php
+++ Protobuf/make_requests.php
@@ -1,15 +1,27 @@
 <?php
 
-use Dotw\Proto\Input;
 use Dotw\Proto\Client;
 use Dotw\Proto\PsfilterRequest;
-
+use Dotw\Proto\PsfilterRequest\RoomOccupancy;
+use Dotw\Proto\PsfilterRequest\SearchPeriodCriteria;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $client = new Client();
+$psfilter = new PsfilterRequest();
+$psfilter->setRoomOcupancy(array(new RoomOccupancy()));
+$psfilter->getRoomOcupancy()[0]->setAdults(1);
+$psfilter->setSearchPeriodCriteria(new SearchPeriodCriteria());
+$psfilter->getSearchPeriodCriteria()->setTravelTo(1);
 
-$req = (new PsfilterRequest())->setPsfilter("PSFILTER")->setRequestSource(1)->setHotelIds(array(1,2,3));
+$psfilter->setPsfilter("PSFILTER")
+        ->setRequestSource(1)
+        ->setHotelIds(array(1,2,3));
+
+
+$req = $psfilter;
+
+
 $reply = $client->psfilter($req);
 
 echo 'PSFILTER = ' . $reply->getReplyString() . PHP_EOL;
--- Protobuf/src/Service.php
+++ Protobuf/src/Service.php
@@ -9,12 +9,15 @@ class Service implements ServerDotwInterface
     
     public function psfilter(\Dotw\Proto\PsfilterRequest $request): PsfilterReply
     {
-        $diff = "KO";
-        $array = $request->getHotelIds();
-        $sizeof = sizeof($array);
-               
-        if ($request->getPsfilter() == "PSFILTER" && $sizeof >= 1) {
-            $diff = "OK";
+        $res = $request->getPsfilter() == "PSFILTER";
+        $res = $res && sizeof($request->getHotelIds()) >= 1;
+        $res = $res && $request->getSearchPeriodCriteria()->getTravelTo() == 1;
+        $res = $res && $request->getRoomOcupancy()[0]->getAdults() == 1;
+        
+                      
+        if ($res) {
+            $size = sizeof($request->getRoomOcupancy());
+            $diff = "$size";
         } else {
             $diff = "KO";
         }
