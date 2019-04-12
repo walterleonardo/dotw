882e3227bcf0bb5bf91172cf932bcddce05b2531
walterleonardo <walterleonardo@gmail.com>
2019-04-12 13:15:41 +0200

    include C protobuf files and classfrom partner

---
+++ secondCall/output/output.php
@@ -0,0 +1,351 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * 
+ * "/usr/bin/php" "/Users/walterleonardo/NetBeansProjects/multinucleo/secondCall/MNtransductor.php"
+
+
+# INPUT OBJECT CREATED #
+
+Hotel\StaticData\StaticInput::__set_state(array(
+   'hotelIds' => 
+  array (
+    71934 => 
+    array (
+      0 => 116104,
+    ),
+  ),
+   'LanguageId' => 1,
+   'ReturnHotelStaticData' => 
+  Hotel\StaticData\ReturnHotelStaticData::__set_state(array(
+     'description1' => true,
+     'description2' => true,
+     'geoPoint' => true,
+     'ratingDescription' => true,
+     'images' => true,
+     'direct' => true,
+     'hotelPreference' => true,
+     'builtYear' => true,
+     'renovationYear' => true,
+     'floors' => true,
+     'noOfRooms' => true,
+     'luxury' => true,
+     'address' => true,
+     'zipCode' => true,
+     'location' => true,
+     'locationId' => true,
+     'location1' => true,
+     'location2' => true,
+     'location3' => true,
+     'stateName' => true,
+     'stateCode' => true,
+     'countryName' => true,
+     'regionName' => true,
+     'regionCode' => true,
+     'amenitie' => true,
+     'leisure' => true,
+     'business' => true,
+     'transportation' => true,
+     'hotelPhone' => true,
+     'hotelCheckIn' => true,
+     'hotelCheckOut' => true,
+     'minAge' => true,
+     'rating' => true,
+     'fireSafety' => true,
+     'chain' => true,
+     'lastUpdated' => true,
+     'transferMandatory' => true,
+     'tariffNotes' => true,
+     'chainName' => true,
+     'hotelProperty' => true,
+     'fullAddress' => true,
+     'exclusive' => true,
+     'attraction' => true,
+     'areaCode' => true,
+     'areaName' => true,
+     'geoLocations' => true,
+  )),
+   'ReturnRoomTypeStaticData' => 
+  Hotel\StaticData\ReturnRoomTypeStaticData::__set_state(array(
+     'roomAmenities' => true,
+     'name' => true,
+     'supplierRoomName' => true,
+     'twin' => true,
+     'roomInfo' => true,
+     'specials' => true,
+     'roomImages' => true,
+     'roomCategory' => true,
+  )),
+   'ReturnRateData' => 
+  Hotel\StaticData\ReturnRateData::__set_state(array(
+     'occupancy' => true,
+     'status' => true,
+     'rateType' => true,
+     'paymentMode' => true,
+     'allowsExtraMeals' => true,
+     'allowsSpecialRequests' => true,
+     'allowsBeddingPreference' => true,
+     'allowsSpecials' => true,
+     'passengerNamesRequiredForBooking' => true,
+     'allocationDetails' => true,
+     'minStay' => true,
+     'dateApplyMinStay' => true,
+     'cancellationRules' => true,
+     'withinCancellationDeadline' => true,
+     'tariffNotes' => true,
+     'isBookable' => true,
+     'onRequest' => true,
+     'total' => true,
+     'dates' => true,
+     'freeStay' => true,
+     'discount' => true,
+     'dayOnRequest' => true,
+     'including' => true,
+     'dailyLeftToSell' => true,
+     'dailyMinStay' => true,
+     'leftToSell' => true,
+     'specials' => true,
+  )),
+))
+
+###
+
+
+# STRING LINE CREATED #
+
+HOTELDATAREQUEST |71934~116104|1|Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y,Y|Y,Y,Y,Y,Y,Y,Y,Y
+
+###
+
+
+
+# ANSWER FROM DAEMON #
+
+OK |||The Norfolk Plaza Hotel is a quiet and attractive hotel ideally located in the heart of London. It is minutes from Paddington Station, and only 15 minutes from Heathrow Airport. The hotel is an easy walking distance from Oxford Street and Hyde Park, and is close to some of the city's best attractions, as well as the West End's vibrant entertainment scene. The hotel is situated in Norfolk Square, a peaceful and attractive square surrounded by beautiful Edwardian buildings. The interior is modern and comfortable and offers a bar and lounges to spend leisure time in, as well as a well-equipped conference room for meetings.-[-The Norfolk Plaza Hotel is a Tourist hotel. Located in City Centre - Paddington area. Shopping is accessible by bus/taxi and the nightlife/restaurants are easily accessible by taxi or bus from the hotel.-[-51.5155797837559,-0.172863006591797-[-This class of accommodations appeals to the more discerning traveler.  Offers a mid-grade level of services, quality, style, and comfort.  Good choice for leisure/business travelers.  Usually offers rooms for meetings and restaurant for dining.-[-Y-[--[-N-[-1989-[-2002-[--[-87-[-N-[-Norfolk Plaza Hotel-[-29/33 Norfolk Square London-[-W2 1 RX-[-City Centre - Paddington-[-16054-[--[--[--[-LONDON-[-14864-[--[--[-UNITED KINGDOM-[-88-[-EUROPE-[-54-[-698-~-1993-~-646-~-719-[--[--[-+44 20 7723 0792-[-14-[-12-[-18-[-561-[-N-[-2019-[-1500964723-[-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48809-#-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48809-#-RwrDLPY4_669492eef20d87e60404a22768761b11.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48819-#-BVUAM0xo_c7e569621f3027d77176e2e235f26472.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48819-#-7cKpKR1Z_64a830999214759642b849ee1e37cc13.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48829-#-RMx6lolT_36d3af46f8ac23af2157a77bc1edae3f.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48839-#-J5dQMmTE_a4c30c8078e4ca95124bb5767458e9ad.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48839-#-6SyExbAJ_cf33cfe49d5dff40fbb98ec4668b35c7.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48839-#-EWyqSBIN_14c712defaff87c34648857924ce2e63.jpg-#--~-O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg-#--#-48849-#-Tl1PIEVB_943a96f6aac4d532fb19412bde1a6b84.jpg-#--[-116104-#-Y-#-729-{-728-{-1958-{-1966-{-2754-{-2764-{-1971-#-SUITE-#-1741~10dpf7~Standard Double Room-{-1741~uzxldt~Standard Single Room-{-1741~1l2vl2~Standard Double or Twin Room-{-1699~1~Single room-{-1741~i7ta2o~Triple Room-{-1659~31~SUITE ROOM-{-1741~1e0ej2~Standard Triple Room 3 Single Beds-{-1659~7~STANDARD ROOM-#-2-{-2-{-0-{-12-{-2-{-N-{-1-{-1-#--{--[--[-0-[-No show or early departure will be subjected to 100% charge.-[-Independent Hotels-[--[-N-[-71934
+
+
+###
+
+
+# PASS ANSWER CHECK #
+
+
+###
+
+
+# PASS DISTRIBUTE VALUES #
+
+
+###
+
+
+array (
+  71934 => 
+  Hotel\StaticData\HotelStaticData::__set_state(array(
+     'RoomTypeStaticDataList' => 
+    array (
+      116104 => 
+      Hotel\StaticData\RoomTypeStaticData::__set_state(array(
+         'twin' => true,
+         'roomAmenities' => 
+        array (
+          0 => '729',
+          1 => '728',
+          2 => '1958',
+          3 => '1966',
+          4 => '2754',
+          5 => '2764',
+          6 => '1971',
+        ),
+         'name' => 'SUITE',
+         'supplierRoomName' => 
+        array (
+          1741 => 
+          array (
+            '10dpf7' => 'Standard Double Room',
+            'uzxldt' => 'Standard Single Room',
+            '1l2vl2' => 'Standard Double or Twin Room',
+            'i7ta2o' => 'Triple Room',
+            '1e0ej2' => 'Standard Triple Room 3 Single Beds',
+          ),
+          1699 => 
+          array (
+            1 => 'Single room',
+          ),
+          1659 => 
+          array (
+            31 => 'SUITE ROOM',
+            7 => 'STANDARD ROOM',
+          ),
+        ),
+         'roomInfo' => 
+        Hotel\StaticData\RoomInfo::__set_state(array(
+           'maxOccupancy' => 2,
+           'maxAdultWithChildren' => 2,
+           'minChildAge' => 0,
+           'maxChildAge' => 12,
+           'maxAdult' => 2,
+           'maxExtraBed' => false,
+           'maxChildren' => 1,
+           'children' => 1,
+        )),
+         'roomCategory' => 
+        Hotel\StaticData\RoomCategory::__set_state(array(
+           'code' => '',
+           'name' => '',
+        )),
+      )),
+    ),
+     'description1' => 'The Norfolk Plaza Hotel is a quiet and attractive hotel ideally located in the heart of London. It is minutes from Paddington Station, and only 15 minutes from Heathrow Airport. The hotel is an easy walking distance from Oxford Street and Hyde Park, and is close to some of the city\'s best attractions, as well as the West End\'s vibrant entertainment scene. The hotel is situated in Norfolk Square, a peaceful and attractive square surrounded by beautiful Edwardian buildings. The interior is modern and comfortable and offers a bar and lounges to spend leisure time in, as well as a well-equipped conference room for meetings.',
+     'description2' => 'The Norfolk Plaza Hotel is a Tourist hotel. Located in City Centre - Paddington area. Shopping is accessible by bus/taxi and the nightlife/restaurants are easily accessible by taxi or bus from the hotel.',
+     'geoPoint' => '51.5155797837559,-0.172863006591797',
+     'ratingDescription' => 'This class of accommodations appeals to the more discerning traveler.  Offers a mid-grade level of services, quality, style, and comfort.  Good choice for leisure/business travelers.  Usually offers rooms for meetings and restaurant for dining.',
+     'images' => 
+    array (
+      0 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48809',
+         'url' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'roomTypeId' => '',
+      )),
+      1 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48809',
+         'url' => 'RwrDLPY4_669492eef20d87e60404a22768761b11.jpg',
+         'roomTypeId' => '',
+      )),
+      2 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48819',
+         'url' => 'BVUAM0xo_c7e569621f3027d77176e2e235f26472.jpg',
+         'roomTypeId' => '',
+      )),
+      3 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48819',
+         'url' => '7cKpKR1Z_64a830999214759642b849ee1e37cc13.jpg',
+         'roomTypeId' => '',
+      )),
+      4 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48829',
+         'url' => 'RMx6lolT_36d3af46f8ac23af2157a77bc1edae3f.jpg',
+         'roomTypeId' => '',
+      )),
+      5 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48839',
+         'url' => 'J5dQMmTE_a4c30c8078e4ca95124bb5767458e9ad.jpg',
+         'roomTypeId' => '',
+      )),
+      6 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48839',
+         'url' => '6SyExbAJ_cf33cfe49d5dff40fbb98ec4668b35c7.jpg',
+         'roomTypeId' => '',
+      )),
+      7 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48839',
+         'url' => 'EWyqSBIN_14c712defaff87c34648857924ce2e63.jpg',
+         'roomTypeId' => '',
+      )),
+      8 => 
+      Hotel\StaticData\ImageData::__set_state(array(
+         'thumb' => 'O1mafGAH_1f8c58757f4c40f7b8a34b39ba2e0a2d.jpg',
+         'alt' => '',
+         'category' => '48849',
+         'url' => 'Tl1PIEVB_943a96f6aac4d532fb19412bde1a6b84.jpg',
+         'roomTypeId' => '',
+      )),
+    ),
+     'direct' => true,
+     'hotelPreference' => NULL,
+     'preferred' => false,
+     'builtYear' => 1989,
+     'renovationYear' => 2002,
+     'floors' => 0,
+     'noOfRooms' => 87,
+     'luxury' => false,
+     'hotelName' => 'Norfolk Plaza Hotel',
+     'address' => '29/33 Norfolk Square London',
+     'zipCode' => 'W2 1 RX',
+     'location' => 'City Centre - Paddington',
+     'locationId' => 16054,
+     'location1' => '',
+     'location2' => '',
+     'location3' => '',
+     'cityName' => 'LONDON',
+     'cityCode' => 14864,
+     'stateName' => '',
+     'stateCode' => 0,
+     'countryName' => 'UNITED KINGDOM',
+     'countryCode' => 88,
+     'regionName' => 'EUROPE',
+     'regionCode' => 54,
+     'amenitie' => 
+    array (
+      0 => '698',
+      1 => '1993',
+      2 => '646',
+      3 => '719',
+    ),
+     'leisure' => 
+    array (
+    ),
+     'business' => 
+    array (
+    ),
+     'transportation' => 
+    array (
+    ),
+     'hotelPhone' => '+44 20 7723 0792',
+     'hotelCheckIn' => 14,
+     'hotelCheckOut' => 12,
+     'minAge' => 18,
+     'rating' => 561,
+     'fireSafety' => false,
+     'chain' => 2019,
+     'lastUpdated' => '2017-07-25 06:38:43',
+     'transferMandatory' => 0,
+     'tariffNotes' => 'No show or early departure will be subjected to 100% charge.',
+     'chainName' => 'Independent Hotels',
+     'hotelProperty' => 0,
+     'fullAddress' => NULL,
+     'attraction' => NULL,
+     'exclusive' => false,
+  )),
+)
+
+For hotel ID 71934		Rooms :1	Images :9	Transportations :0
+## Hotels received 1 ##
+## Rooms 1 ##
+## Images 9 ##
+## Transportation 0 ##
+
+REQUEST = 1
+ERRORS = 0
+TIME ELAPSED = 0.068675994873047
+ * 
+ */
+
