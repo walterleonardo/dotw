50387cf8fc9a4426810976b3cf12d81d538d03ee
walterleonardo <walterleonardo@gmail.com>
2019-04-12 13:09:32 +0200

    include C protobuf files and classfrom partner

--- firstCall/MNtransductorClass.php
+++ firstCall/MNtransductorClass.php
@@ -34,7 +34,7 @@ class ArrayChannelCodes {
 /*
  * You need replace this previous require for your objets files
  */
-require 'classFromPartner_wps92_phase_2_wps129_1_1.php';
+require 'classFromPartner_wps92_phase_2_wps129.php';
 
 
 if ($platform == 'test')
