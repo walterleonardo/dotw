c17671d26f6e3663bef3dfa47711af755ead0c7e
walterleonardo <walterleonardo@gmail.com>
2019-04-23 12:08:57 +0200

    protobuf included second call into the same proto file

---
+++ Protobuf/genProto/Dotw/SecondCall/classFromPartner_Demo_jiraWPS92_phase_2_wps112.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Hotel\StaticData;
+
+class StaticInput {
+//941895 //139534
+    public $hotelIds = array (
+    2434 => 
+    array (
+   0 => 133414
+                    ,1 => 133424
+                    ,2 => 133434
+                    ,3 => 133444
+                    ,4 => 133454
+                    ,5 => 133464
+                    ,6 => 133474
+                    ,7 => 133484
+                    ,8 => 133494
+                    ,9 => 133504
+                    ,10 => 137316
+                    ,11 => 16145218
+                    ,12 => 16145228
+                    ,13 => 16145238
+                    ,14 => 16145248
+                    ,15 => 16145258
+                    ,16 => 16145268
+                    ,17 => 16145278
+                    ,18 => 16145288
+                    ,19 => 16145298
+                    ,20 => 16145308
+                    ,21 => 16145318
+                    ,22 => 16145328
+                    ,23 => 35211535
+                    ,24 => 56017195
+                    ,25 => 56017205
+                    ,26 => 56017215
+                    ,27 => 64514355
+                    ,28 => 64514365
+                    ,29 => 64514375
+                    ,30 => 64514385
+                    ,31 => 64514395
+                    ,32 => 64514405
+                    ,33 => 64514415
+                    ,34 => 64514425
+                    ,35 => 64514435
+                    ,36 => 64514445
+                    ,37 => 64514455
+                    ,38 => 64514465
+                    ,39 => 64514475
+                    ,40 => 76528115
+                    ,41 => 76531395
+     
+    ),
+    );
+    public $LanguageId = 1;
+    //MANDATORY ARRAY
+    /**
+     * @var ReturnHotelStaticData;
+     */
+    public $ReturnHotelStaticData = array();
+
+    /**
+     * @var ReturnRoomTypeStaticData;
+     */
+    public $ReturnRoomTypeStaticData = array();
+
+    /**
+     * @var ReturnRateData;
+     */
+    public $ReturnRateData = array();
+
+    function __construct() {
+        $this->ReturnHotelStaticData = new ReturnHotelStaticData();
+        $this->ReturnRoomTypeStaticData = new ReturnRoomTypeStaticData();
+        $this->ReturnRateData = new ReturnRateData();
+    }
+
+}
+
+class ReturnHotelStaticData {
+    public $description1 = false; //NO MANDATORY BOOL
+    public $description2 = true;
+    public $geoPoint = true;
+    public $ratingDescription = true;
+    public $images = true;
+    public $direct = true;
+    public $hotelPreference = true;
+    public $builtYear = true;
+    public $renovationYear = true;
+    public $floors = true;
+    public $noOfRooms = true;
+    public $luxury = true;
+    public $address = true;
+    public $zipCode = true;
+    public $location = true;
+    public $locationId = true;
+    public $location1 = true;
+    public $location2 = true;
+    public $location3 = true;
+    public $stateName = true;
+    public $stateCode = true;
+    public $countryName = true;
+    public $regionName = true;
+    public $regionCode = true;
+    public $amenitie = true;
+    public $leisure = true;
+    public $business = true;
+    public $transportation = true;
+    public $hotelPhone = true;
+    public $hotelCheckIn = true;
+    public $hotelCheckOut = true;
+    public $minAge = true;
+    public $rating = true;
+    public $fireSafety = true;
+    public $chain = true;
+    public $lastUpdated = true;
+    public $transferMandatory = true;
+    public $tariffNotes = true;
+    public $chainName = true;
+    public $hotelProperty = true;
+    public $fullAddress = true;
+    public $exclusive = true;
+    public $attraction = true;//Future develop
+    public $areaCode = false;//Future develop
+    public $areaName = false;//Future develop
+    public $geoLocations = false;//Future develop
+    public $priority = false;//Future develop
+}
+
+class ReturnRoomTypeStaticData {
+    public $roomAmenities = false;
+    public $name = true;
+    public $supplierRoomName = true;
+    public $twin = true; //NO MANDATORY BOOL
+    public $roomInfo = false;
+    public $specials = true;
+    public $roomImages = false;
+    public $roomCategory = true; //new attribute
+}
+
+class ReturnRateData {
+    public $occupancy = true;
+    public $status = true;
+    public $rateType = true;
+    public $paymentMode = true;
+    public $allowsExtraMeals = true;
+    public $allowsSpecialRequests = true;
+    public $allowsBeddingPreference = true;
+    public $allowsSpecials = true;
+    public $passengerNamesRequiredForBooking = true;
+    public $allocationDetails = true;
+    public $minStay = true;
+    public $dateApplyMinStay = true;
+    public $cancellationRules = true;
+    public $withinCancellationDeadline = true;
+    public $tariffNotes = true;
+    public $isBookable = true;
+    public $onRequest = true;
+    public $total = true;
+    public $dates = true;
+    public $freeStay = true;
+    public $discount = true; 
+    public $dayOnRequest = true;
+    public $including = true;
+    public $dailyLeftToSell = true;
+    public $dailyMinStay = true;
+    public $leftToSell = true; 
+    public $specials = true;
+}
--- Protobuf/make_requests.php
+++ Protobuf/make_requests.php
@@ -7,9 +7,12 @@ use Protobuffer\Dotwproto\PSFReply;
 
 require __DIR__ . '/vendor/autoload.php';
 require "genProto/Dotw/FirstCall/classFromPartner_wps92_phase_2_wps129.php";
+require "genProto/Dotw/SecondCall/classFromPartner_Demo_jiraWPS92_phase_2_wps112.php";
 use Dotw\FirstCall\Input;
+use Hotel\StaticData\StaticInput;
 
 
+//FIRST CALL REQUEST
 function managerSupplierRequest(Input &$inputObj)
     {
     //organize types
@@ -87,3 +90,81 @@ function managerSupplierRequest(Input &$inputObj)
 $inputPresupplier = new \Dotw\FirstCall\Input;
 $answerRequest = managerSupplierRequest($inputPresupplier);
 
+
+////SECOND CALL REQUEST 
+//function managerHotelRequest(StaticInput &$inputObj)
+//    {
+//    //organize types
+//        $aRoomOccupancy = $inputObj->RoomOccupancy;
+//        $aRoomTypeFilters = $inputObj->RoomTypeFilters;
+//        if ($aRoomTypeFilters != NULL)
+//            $aRoomCategories = $inputObj->RoomTypeFilters->roomCategories;
+//        else
+//            $aRoomCategories = array();
+//        
+//        $aHotelFilters = $inputObj->HotelFilters;
+//        $aSearchPeriodCriteria = $inputObj->SearchPeriodCriteria;
+//        
+//        
+//        
+//        if($inputObj->city.fin)
+//        {
+//            
+//        }
+//        
+//       //Distribute values 
+//        $client = new Client();
+//        $psfilter = new PSFRequest();
+//        $psfilter->setPsfilter("PSFPROTO")
+//                ->setCustomerId($inputObj->customerId)
+//                ->setEnvironment($inputObj->environment)
+//                ->setRequestSource($inputObj->requestSource)
+//                ->setExceptRestrictions($inputObj->exceptRestrictions)
+//                ->setPassengerNationalityOrResidenceProvided($inputObj->passengerNationalityOrResidenceProvided)
+//                ->setHotelIds($inputObj->hotelIds)
+//                ->setBookingChannelsWithAutoMapping($inputObj->bookingChannelsWithAutoMapping)
+//                ->setBookingChannelTypes($inputObj->bookingChannelTypes)
+//                ->setExcludedBookingchannel($inputObj->excludedBookingchannel)
+//                ->setActiveForRoomCategories($inputObj->activeForRoomCategories)
+//                ->setAdditionalFilters($inputObj->AdditionalFilters);
+//        
+//        
+//        $psfilter->setRoomOccupancy(array(
+//            (new PSFRequest\RoomOccupancy())->setAdults(2)->setChildren(array())->setTwin(false)->setExtraBed(false)
+//         //,(new PSFRequest\RoomOccupancy())->setAdults(3)->setChildren(array(1,2,3))->setTwin(false)->setExtraBed(false)
+//        ));
+//        
+//        
+//        $psfilter->setSearchPeriodCriteria(new PSFRequest\SearchPeriodCriteria());
+//        $psfilter->getSearchPeriodCriteria()->setTravelFrom(1552640400);
+//        $psfilter->getSearchPeriodCriteria()->setTravelTo(1553644800);
+//        $psfilter->getSearchPeriodCriteria()->setBookingDateTime(1552694400);
+//        
+//        
+//        //$psfilter->setHotelFilters(new PSFRequest\HotelFilters());
+//        //$psfilter->getHotelFilters()->setLuxury(111);
+//        
+//        
+//        //$psfilter->setRoomTypeFilters(new PSFRequest\RoomTypeFilters());
+////        $psfilter->getRoomTypeFilters()->setSuite(1);
+////        $psfilter->getRoomTypeFilters()->setRoomAmenitie(array(1,2,3));
+////        $psfilter->getRoomTypeFilters()->setroomId(array(1,2,3));
+////        $psfilter->getRoomTypeFilters()->setroomName("ROOM");
+////        $psfilter->getRoomTypeFilters()->setRoomCategories(array(
+////            (new PSFRequest\RoomTypeFilters\RoomCategory())->setMainCategory(1)->setSubCategory(2)->setView(3)->setBeddingType(4)->setAttribute1(5)->setAttribute2(6),
+////            (new PSFRequest\RoomTypeFilters\RoomCategory())->setMainCategory(11)->setSubCategory(22)->setView(33)->setBeddingType(44)->setAttribute1(55)->setAttribute2(66)));
+//        
+//        //Call to server and get answer
+//        $reply = $client->psfilter($psfilter);
+//
+//        if ($reply->getReplyString() == "") 
+//        {
+//            echo "No Server Live";
+//        } else {
+//            echo 'PSFILTER = ' . $reply->getReplyString() . PHP_EOL;
+//        }
+//       
+//    }
+//
+//$inputHotelData= new \Hotel\StaticData\StaticInput;
+//$answerRequest = managerHotelRequest($inputHotelData);
